96ba278f302efba0290d39c1c986e7242a388bd2
benji <benjamincomley@gmail.com>
2017-11-07 12:22:47 +0100

    add wont update without image test

--- spec/features/editing_posts_spec.rb
+++ spec/features/editing_posts_spec.rb
@@ -16,4 +16,11 @@ feature 'Editing posts' do
     expect(page).to have_content("Post updated hombre")
     expect(page).to have_content("Oh god, you weren't meant to see this picture!")
   end
+
+  scenario "won't update a post without an image" do
+    attach_file('Image', 'spec/files/coffee.zip')
+    click_button 'Update Post'
+
+    expect(page).to have_content("Something is wrong with your form!")
+  end
 end
